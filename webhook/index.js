943517b49c275b4d19b9c8089976ed4becc0c792
SnowCait <SnowCait@users.noreply.github.com>
2019-01-20 06:29:54 +0900

    Retrieve sha

--- webhook/index.js
+++ webhook/index.js
@@ -6,7 +6,17 @@ module.exports = async function (context, req) {
     // https://developer.github.com/webhooks/#events
     const githubEvent = req.headers['x-github-event'];
 
+    if (githubEvent !== 'check_suite') {
+        context.res = {
+            body: `Webhook ${githubEvent} success.`
+        };
+        return;
+    }
+
+    const headSha = req.body.check_suite.pull_requests.head.sha;
+    const baseSha = req.body.check_suite.pull_requests.base.sha;
+
     context.res = {
-        body: `Webhook ${githubEvent} success.`
+        body: `Webhook ${githubEvent} success.\nhead: ${headSha}\nbase: ${baseSha}`
     };
 };
