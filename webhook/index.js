34b7e2d45c6cae308190ff3f2199deb2e300f207
SnowCait <SnowCait@users.noreply.github.com>
2019-01-20 04:21:43 +0900

    Output event log

--- webhook/index.js
+++ webhook/index.js
@@ -1,5 +1,6 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
+    context.log(JSON.stringify(req.headers));
 
     const event = req.headers['X-GitHub-Event'];
 
