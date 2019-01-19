a859041c77a0bbd81af4b1d9e04cf7bdb853c961
SnowCait <SnowCait@users.noreply.github.com>
2019-01-20 05:12:20 +0900

    Output event log

--- webhook/index.js
+++ webhook/index.js
@@ -3,9 +3,9 @@ module.exports = async function (context, req) {
     context.log(JSON.stringify(req.headers));
 
     // https://developer.github.com/webhooks/#events
-    const event = req.headers['x-github-event'];
+    const githubEvent = req.headers['x-github-event'];
 
     context.res = {
-        body: `Webhook ${event} success.`
+        body: `Webhook ${githubEvent} success.`
     };
 };
